59668308ebbe53c3ce851316bd3084e662b50e2d
Rinat Tagirov <rtagirov@login-4.rcs.ic.ac.uk>
2021-10-13 10:47:22 +0100

    minor changes

--- pyt/copy_conv.py
+++ pyt/copy_conv.py
@@ -2,23 +2,18 @@ import os
 
 from tqdm import tqdm
 
-f = open('fail.log', 'r')
+f = open('success.log', 'r')
 
 lines = f.readlines()
 
 for l in tqdm(lines):
 
-    if 'convergence' in l:
+    group_ray = l.split(';')[0]
 
-       group_ray = l.split(';')[0]
+    remainder = l.split(';')[1]
 
-       remainder = l.split(';')[1]
+    ij = remainder.split(':')[0]
 
-       ij = remainder.split(':')[0]
-
-#       i = ij.split('.')[0]
-#       j = ij.split('.')[1]
-
-       os.system('cp ./groups/' + group_ray + '/conv.out ./noconv/' + ij)
+    os.system('cp ./groups/' + group_ray + '/conv.out ./conv/' + ij)
 
 f.close()
